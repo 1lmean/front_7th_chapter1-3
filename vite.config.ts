4b299bf75d5e92b68c0e38b80db8897d75fb0870
1lmean <seonmin0215@icloud.com>
2025-11-06 14:29:21 +0900

    chore: vite config에서 playwright 관련 디렉토리 제외

--- vite.config.ts
+++ vite.config.ts
@@ -16,6 +16,15 @@ export default defineConfig({
     testTimeout: 20000,
     environment: 'jsdom',
     setupFiles: './src/setupTests.ts',
+    exclude: [
+      '**/node_modules/**',
+      '**/dist/**',
+      '**/cypress/**',
+      '**/.{idea,git,cache,output,temp}/**',
+      '**/{karma,rollup,webpack,vite,vitest,jest,ava,babel,nyc,cypress,tsup,build,eslint,prettier}.config.*',
+      '**/playwright/**',
+      '**/__tests__/playwright/**',
+    ],
     coverage: {
       reportsDirectory: './.coverage',
       reporter: ['lcov', 'json', 'json-summary'],
